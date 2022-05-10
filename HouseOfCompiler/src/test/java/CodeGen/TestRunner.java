39d76e3ff146906a44f061d7fa9dd98d24b879ee
raphaelsack <i20029@hb.dhbw-stuttgart.de>
2022-05-10 19:43:12 +0200

    changed testing

---
+++ HouseOfCompiler/src/test/java/CodeGen/TestRunner.java
@@ -0,0 +1,35 @@
+package CodeGen;
+
+import Helper.ReflectLoader;
+import common.Compiler;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import syntaxtree.structure.ClassDecl;
+import syntaxtree.structure.Program;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Vector;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("EmptyClass TAS -> Bytecode")
+public class TestEmptyClass {
+
+    @Test
+    @DisplayName("main")
+    void main() throws ReflectiveOperationException {
+        ClassDecl emptyClass = new ClassDecl("EmptyClass", new Vector<>(),new Vector<>(),new Vector<>());
+        Vector<ClassDecl> classDecls = new Vector<>();
+        classDecls.add(emptyClass);
+
+
+        var tast = new Program(classDecls);
+
+        var bc = Compiler.getFactory().getProgramGenerator().generateBytecode(tast);
+        ReflectLoader loader = new ReflectLoader(bc);
+        Class c = loader.findClass("EmptyClass");
+        Object o = c.getDeclaredConstructor().newInstance();
+        assertEquals(o.getClass().getName(), "EmptyClass");
+    }
+
+}
