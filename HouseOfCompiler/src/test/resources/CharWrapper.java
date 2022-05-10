adf360806c8aecab67e6af781b5c2deb4ab589fb
raphaelsack <i20029@hb.dhbw-stuttgart.de>
2022-05-10 16:02:45 +0200

    Added Test Resourcefiles

---
+++ HouseOfCompiler/src/test/resources/CharWrapper.java
@@ -0,0 +1,17 @@
+class IntegerWrapper{
+
+    private int i;
+
+    public IntegerWrapper(int i){
+        this.i = i;
+    }
+
+    public void setNumber(int i){
+        this.i = i;
+    }
+
+    public int getNumber(){
+        return this.i;
+    }
+
+}
---
+++ HouseOfCompiler/src/test/resources/VoidMethodReturn.java
@@ -0,0 +1,8 @@
+class VoidMethod{
+
+
+    void test(){
+        
+    }
+
+}
