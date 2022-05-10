8dd7db9b1fa5b1623f366baef83825c9cbe45ef6
raphaelsack <i20029@hb.dhbw-stuttgart.de>
2022-05-10 18:44:16 +0200

    added test suite

--- HouseOfCompiler/src/test/java/AST/TestEmptyClass.java
+++ HouseOfCompiler/src/test/java/AST/TestEmptyClass.java
@@ -1,35 +1,29 @@
 package AST;
 
-import Helper.Resources;
+import common.Compiler;
 import org.junit.jupiter.api.Test;
 import syntaxtree.structure.*;
 
-import java.io.File;
-import java.io.FileInputStream;
+import Helper.Resources;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.Vector;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class EmptyClass {
+public class TestEmptyClass {
     @Test
     void main() throws FileNotFoundException {
 
         InputStream file = Resources.getFileAsStream("EmptyClass.java");
-
-
-
+        Program tast = Compiler.getFactory().getAstGenerator().getAst(file);
 
         Vector<ClassDecl> classDecls = new Vector<ClassDecl>();
         ClassDecl classDecl = new ClassDecl("EmptyClass", new Vector<FieldDecl>(),new Vector<MethodDecl>(),new Vector<ConstructorDecl>());
         classDecls.add(classDecl);
-
         var testProgram = new Program(classDecls);
 
-
-
-        assertEquals(testProgram, testProgram);
+        assertEquals(tast, testProgram);
 
     }
 
