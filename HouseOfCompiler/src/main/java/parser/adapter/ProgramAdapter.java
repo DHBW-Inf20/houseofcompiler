fd1e9db679e43408b3fdd382336b675232bedcfb
Lukas-EppleDHBW <i20009@hb.dhbw-stuttgart.de>
2022-05-10 18:23:50 +0200

    implemented adapters for parsing empty class

--- HouseOfCompiler/src/main/java/parser/adapter/ProgramAdapter.java
+++ HouseOfCompiler/src/main/java/parser/adapter/ProgramAdapter.java
@@ -1,7 +1,18 @@
 package parser.adapter;
 
 
+import parser.generated.JavaSubsetParser;
+import syntaxtree.structure.ClassDecl;
+import syntaxtree.structure.Program;
+
+import java.util.Vector;
+
 public class ProgramAdapter {
 
-    public Program 
+    public static Program adapt(JavaSubsetParser.ProgramContext programContext){
+        Vector<ClassDecl> classes = new Vector<ClassDecl>();
+        programContext.classdecl().forEach(classContext -> classes.add(ClassAdapter.adapt(classContext)));
+
+        return new Program(classes);
+    }
 }
