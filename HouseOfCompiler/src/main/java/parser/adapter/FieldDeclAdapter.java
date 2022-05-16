19ff83cc4ce24ec016b707aa444625bd6c5c8f4b
LukasEEH <lukas.epple25@web.de>
2022-05-16 23:22:19 +0200

    parser typing of method-, localvar and field decls

--- HouseOfCompiler/src/main/java/parser/adapter/FieldDeclAdapter.java
+++ HouseOfCompiler/src/main/java/parser/adapter/FieldDeclAdapter.java
@@ -6,20 +6,22 @@ import syntaxtree.structure.FieldDecl;
 
 import java.util.Locale;
 
-public class FieldAdapter {
+public class FieldDeclAdapter {
     public static FieldDecl adapt(JavaSubsetParser.FieldDeclContext fieldDeclContext){
         if (fieldDeclContext.AccessModifier() != null){
             return new FieldDecl(
-                    fieldDeclContext.Identifier().getText(),
                     AccessModifier.valueOf(
                             fieldDeclContext.AccessModifier().getText().toUpperCase(Locale.ROOT)
-                    )
+                    ),
+                    TypeAdapter.adapt(fieldDeclContext.type()),
+                    fieldDeclContext.Identifier().getText()
             );
         }
         else {
             return new FieldDecl(
-                    fieldDeclContext.Identifier().getText(),
-                    AccessModifier.PACKAGE_PRIVATE
+                    AccessModifier.PACKAGE_PRIVATE,
+                    TypeAdapter.adapt(fieldDeclContext.type()),
+                    fieldDeclContext.Identifier().getText()
             );
         }
     }
