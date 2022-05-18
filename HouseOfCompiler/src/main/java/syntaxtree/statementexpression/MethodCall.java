f7f9cc83c827a3b5cee4da6a9434e2b80b046766
LukasEEH <lukas.epple25@web.de>
2022-05-18 14:33:45 +0200

    receiver falsch geschrieben lul

--- HouseOfCompiler/src/main/java/syntaxtree/statementexpression/MethodCall.java
+++ HouseOfCompiler/src/main/java/syntaxtree/statementexpression/MethodCall.java
@@ -12,32 +12,32 @@ import visitor.codevisitor.MethodCodeVisitor;
 public class MethodCall implements IStatementExpression{
 
     public String identifier;
-    public IExpression reciever;
+    public IExpression receiver;
     public PrintableVector<IExpression> arguments;
     public Type type;
 
-    public MethodCall(String identifier, IExpression target, PrintableVector<IExpression> arguments) {
+    public MethodCall(String identifier, IExpression receiver, PrintableVector<IExpression> arguments) {
         this.identifier = identifier;
-        this.reciever = target;
+        this.receiver = receiver;
         this.arguments = arguments;
     }
 
-    public MethodCall(IExpression target, String identifier,  PrintableVector<IExpression> arguments) {
+    public MethodCall(IExpression receiver, String identifier,  PrintableVector<IExpression> arguments) {
         this.identifier = identifier;
-        this.reciever = target;
+        this.receiver = receiver;
         this.arguments = arguments;
     }
 
-    public MethodCall(Type type, IExpression target, String identifier, PrintableVector<IExpression> arguments) {
+    public MethodCall(Type type, IExpression receiver, String identifier, PrintableVector<IExpression> arguments) {
         this.identifier = identifier;
-        this.target = target;
+        this.receiver = receiver;
         this.arguments = arguments;
         this.type = type;
     }
 
     public MethodCall(String identifier, PrintableVector<IExpression> arguments) {
         this.identifier = identifier;
-        this.reciever = null;
+        this.receiver = null;
         this.arguments = arguments;
     }
 
@@ -45,8 +45,8 @@ public class MethodCall implements IStatementExpression{
         return identifier;
     }
 
-    public IExpression getReciever() {
-        return reciever;
+    public IExpression getReceiver() {
+        return receiver;
     }
 
     public PrintableVector<IExpression> getArguments() {
@@ -76,11 +76,11 @@ public class MethodCall implements IStatementExpression{
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         MethodCall that = (MethodCall) o;
-        return identifier.equals(that.identifier) && reciever.equals(that.reciever) && arguments.equals(that.arguments) && Objects.equals(type, that.type);
+        return identifier.equals(that.identifier) && receiver.equals(that.receiver) && arguments.equals(that.arguments) && Objects.equals(type, that.type);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(identifier, reciever, arguments, type);
+        return Objects.hash(identifier, receiver, arguments, type);
     }
 }
