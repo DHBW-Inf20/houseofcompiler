cc5a5bbcddcc43eca5e34309f87b433fd7faaabe
Fabian <fabian.klimpel2002@gmail.com>
2022-05-21 16:35:46 +0200

    fix ValueAdapter

--- HouseOfCompiler/src/main/resources/Test.java
+++ HouseOfCompiler/src/main/resources/Test.java
@@ -1,32 +1,39 @@
 class Test {
 
+//    int main() {
+//        LinkedList list = new LinkedList(1);
+//        list.add(2);
+//        list.add(3);
+//        list.add(4);
+//        return 1;
+//    }
+
     int main() {
-        LinkedList list = new LinkedList(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        return 1;
-    }
-
-}
-
-class LinkedList {
-
-    int x;
-    private LinkedList next;
-
-    private LinkedList nullList;
-
-    public LinkedList(int x) {
-        this.x = x;
-    }
-
-    void add(int x) {
-        if (next == nullList) {
-            next = new LinkedList(x);
+        if (true == false) {
+            return 1;
         } else {
-            next.add(x);
+            return 0;
         }
     }
+}
+
+//class LinkedList {
+//
+//    int x;
+//    private LinkedList next;
+//
+//    private LinkedList nullList;
+//
+//    public LinkedList(int x) {
+//        this.x = x;
+//    }
+//
+//    void add(int x) {
+//        if (next == nullList) {
+//            next = new LinkedList(x);
+//        } else {
+//            next.add(x);
+//        }
+//    }
 
 }
